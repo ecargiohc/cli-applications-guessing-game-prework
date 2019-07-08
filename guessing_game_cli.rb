fffa8a74e3db45f6178e5aefadbe8cbd27b33d9e
Grace Colette <ilovegrace09@gmail.com>
2019-07-08 22:14:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
     elsif input == "exit"
       puts "Goodbye!"
-    elsif 
+    else 
       puts "Sorry! The computer guessed #{rando}."
   end
 end
